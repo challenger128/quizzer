07ff1caf1d8e72a5495f8dc4473658572900caa3
challenger128 <dchallenger128@gmail.com>
2023-05-17 08:58:39 +0500

    fixed backend

--- backend/app/api/api_v1/handlers/result.py
+++ backend/app/api/api_v1/handlers/result.py
@@ -9,13 +9,17 @@ from app.schemas.result_schema import ResultCreate, ResultOut
 
 result_router = APIRouter()
 
-@result_router.post('/', summary="Create result", response_model=ResultOut)
+@result_router.post('/', summary="Create a result", response_model=ResultOut)
 async def create_result(data: ResultCreate):
     return await ResultService.create_result(data)
 
 @result_router.get('/{quiz.id}', summary="List results related to some quiz", response_model=List[QuizResult])
 async def list(quiz_id: UUID, current_user: User = Depends(get_current_user)):
     return await ResultService.list_results(quiz_id, current_user)
+
+@result_router.get('/{result.id}', summary="Get a result yby its id", response_model=QuizResult)
+async def get_result(result_id: UUID, current_user: User = Depends(get_current_user)):
+    return await ResultService.retrieve_result(result_id, current_user)
 
 @result_router.delete('/{result.id}', summary="Delete result by its id")
 async def delete(result_id: UUID, current_user: User = Depends(get_current_user)):
